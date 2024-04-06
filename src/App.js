5d7819609f9ee9f651e763f87211a6733f39e073
bobbyhadz <opmftw@gmail.com>
2024-04-06 13:31:03 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          marginLeft: '4rem',
+          fontSize: '20px',
+          padding: '20px',
+          backgroundColor: 'salmon',
+          color: 'white',
+        }}
+      >
+        Hello world
+      </h1>
+    </div>
+  );
+};
+
+export default App;
